2b6fc85c6f38a156850383f57a9eec91859fc559
xuejike <xuejike2004@126.com>
2017-06-26 14:37:09 +0800

    Spring  Boot Starter 配置

--- src/main/java/com/bidanet/bdcms/core/AutoCoreConfig.java
+++ src/main/java/com/bidanet/bdcms/core/AutoCoreConfig.java
@@ -10,12 +10,14 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import javax.annotation.PostConstruct;
 import java.nio.charset.Charset;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -35,6 +37,12 @@ public class AutoCoreConfig extends WebMvcConfigurerAdapter{
     public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
         FastJsonHttpMessageConverter fastJsonHttpMessageConverter = new FastJsonHttpMessageConverter();
         fastJsonHttpMessageConverter.setCharset(Charset.forName("UTF-8"));
+        ArrayList<MediaType> mediaTypes = new ArrayList<>();
+//        mediaTypes.add(MediaType.valueOf(MediaType.APPLICATION_JSON_UTF8_VALUE));
+        mediaTypes.add(MediaType.APPLICATION_JSON_UTF8);
+        mediaTypes.add(MediaType.TEXT_HTML);
+        fastJsonHttpMessageConverter.setSupportedMediaTypes(mediaTypes);
+
         fastJsonHttpMessageConverter.setFeatures(SerializerFeature.DisableCircularReferenceDetect);
         converters.add(new FileOutputMessageConverter());
         converters.add(new FastJsonHttpMessageConverter());
